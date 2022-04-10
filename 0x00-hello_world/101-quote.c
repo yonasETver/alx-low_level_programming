690190146314c450ade95fec29620120f92c184d
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-10 23:02:28 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,6 +2,6 @@
 
 int main(void) 
 {
-fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+std::cout<<"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 return (1);
 }
