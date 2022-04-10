0df0bd6d775e9b9b513a7ed60870e96bad611aed
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-10 22:59:38 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) 
-{ 
-fwrite(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 return (1);
 }
