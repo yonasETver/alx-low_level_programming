91571c01d304b0c33af5213012c069f301c43117
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-10 22:41:20 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+void trim(char *s);
+
 int main(void) /*this is were the main program start runing*/
 {
+trim(s);
 int a; long int b; long long int c; char d; 
 float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
@@ -10,4 +13,16 @@ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
+}
+
+void trim (char *s)
+{
+  int i =strlen(s)-1;
+  while (i>0)
+  {
+    if(s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+      i--;
+    else break; 
+  }
+  s[i+1]='\0';
 }
