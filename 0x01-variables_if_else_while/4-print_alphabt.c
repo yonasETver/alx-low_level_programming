76b2ae6294129769c9551b55d7ae6f028c1d256e
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-11 20:29:37 +0300

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet except the letters q and e.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char x = 'a';
+
+  for(;x <= 'z';x++)
+	{
+		if (x != 'q' && x != 'e')
+		{
+			putchar(x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
