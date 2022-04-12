8a62f50af872d18bea18d2f32eb1b34028f1bd15
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-12 13:56:15 +0300

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  * main - print the first 50 fibonacci numbers starting with 1 and 2.
+  * Return: Nothing.
+  */
+int main(void)
+{
+	unsigned long count, i, j, k;
+
+	i = 0;
+	j = 1;
+  count = 0;
+	while (count < 50)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu", k);
+		if (count == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+    count++;
+	}
+	return (0);
+}
