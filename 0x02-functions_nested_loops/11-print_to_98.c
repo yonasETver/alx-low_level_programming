8d1e312da2b663c59d0d32099e936414e5bc2b8f
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-12 13:45:04 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,24 +10,26 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (; n <= 98; n++)
+		while (n <= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
 			{
 				printf(", ");
 			}
+			n++;
 		}
 	}
 	else if (n > 98)
 	{
-		for (n; n >= 98; n--)
+		while (n >= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
 			{
 				printf(", ");
 			}
+			n--;
 		}
 	}
 	else
