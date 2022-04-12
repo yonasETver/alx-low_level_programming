53e52ef5d87d88fc69309fe1ae91c41a7a454ad5
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-12 13:30:21 +0300

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - a function that sum two integers and returns the thire result
+ * @num_one: first integer number input
+ * @num_two: second integer number input
+ * Return: num_one + num_two
+ */
+int add(int num_one, int num_two)
+{
+	return (num_one + num_two);
+}
