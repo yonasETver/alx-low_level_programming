1c53118d8247a556dc03065b7c49c02977b5acf9
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-15 15:23:58 +0300

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_diagonal -  checks for checks for a digit (0 through 9).
+ * @n: n -  Variable
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n > 0)
+	{
+    x = 1;
+		while (x <= n)
+		{
+      y = 1;
+			while (y <= n)
+			{
+				if (x == y)
+				{
+					_putchar(92);
+					break;
+				}
+				_putchar(' ');
+        y++;
+			}
+			_putchar('\n');
+      x++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
