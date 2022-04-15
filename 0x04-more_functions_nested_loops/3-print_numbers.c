f780b8eb0b89ce83a7d0f7d09299858caf9f81a1
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-15 15:11:25 +0300

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers -  checks for checks for a digit (0 through 9).
+ * @x: a variable 
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int x=48;
+
+	while (x < 58)
+	{
+		_putchar(x);
+    x++;
+    
+	}
+	_putchar('\n');
+}
