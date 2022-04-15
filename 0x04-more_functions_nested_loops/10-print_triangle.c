ba71c8b58d742544f5e08fc69434977d08fa6a9e
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-15 15:48:38 +0300

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+include "main.h"
+
+/**
+ * print_triangle -  checks for checks for a digit (0 through 9).
+ * @size: size -  Variable
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+    x = 1;
+		while (x <= size)
+		{
+      y = 1;
+			while (y <= size)
+			{
+				if (y <= size - x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+        y++;
+			}
+			_putchar('\n');
+      x++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
