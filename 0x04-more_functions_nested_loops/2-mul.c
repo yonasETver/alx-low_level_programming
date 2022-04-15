5cfd2f39f134d03433fa435ff4566899bd1ac571
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-15 15:04:44 +0300

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul -  checks for checks for a digit (0 through 9).
+ * @x: x -  Variable hold integer
+ * @y: y - variable hold integer
+ * Return: Always 0.
+ */
+int mul(int x, int y)
+{
+
+	int product;
+
+	product = x * y;
+
+	return (product);
+}
