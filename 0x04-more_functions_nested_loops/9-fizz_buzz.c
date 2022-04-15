baf0c0f42f48f9fca90b46f68847e15b0c44ffd3
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-15 15:42:33 +0300

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main -  checks for checks for a digit (0 through 9).
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x;
+  x = 1;
+	while (x <= 100)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			if (x % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (x % 5 == 0)
+			{
+				printf("Buzz");
+			}
+      x++;
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
