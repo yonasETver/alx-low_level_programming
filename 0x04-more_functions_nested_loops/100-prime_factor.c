fc84f7479e12548de1d5e3a7e89181cc213792ec
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-15 15:59:59 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,15 +8,14 @@
 int main(void)
 {
 	unsigned long int i, n = 612852475143;
-  
-  i = 3;
-	for (i < 782849)
+	i = 3;
+	while (i < 782849)
 	{
 		while ((n % i == 0) && (n != i))
-    {
+		{
 			n = n / i;
-    }
-    i = i + 2
+		}
+		i = i + 2
 	}
 	printf("%lu\n", n);
 	return (0);
