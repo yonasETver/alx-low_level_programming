b1c9d941b7b0bb5bc9b13a7051b5a748dc2b6149
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-15 16:25:15 +0300

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 01356789\n
+ *
+ * Return: void
+ */
+
+
+void print_most_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+    c++;
+	}
+	_putchar('\n');
+}
