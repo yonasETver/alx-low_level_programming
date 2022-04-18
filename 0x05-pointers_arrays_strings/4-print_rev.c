5fbefb26720cf45d96af1fbdf98c27ea777331b1
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-18 14:11:55 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_rev - function prints a string, in reverse order
- * str_len - used to count string length
- * @str: variable of string
+ * print_rev - a function that prints a string, in reverse
+ * fcounter is to first count to end, n is to count back
+ * @s: str input
+ * Return: string in reverse
  */
 void print_rev(char *s)
 {
