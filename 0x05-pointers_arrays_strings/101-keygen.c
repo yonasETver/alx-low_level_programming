323008750b8fb67f71735aa45381e18689aa469a
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-18 15:11:32 +0300

    Create 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates random valid passwords for the program 101-crackme.
+ */
+int main(void)
+{
+char seed[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char password[22];
+int i, n, passw, complem;
+
+srand(time(0));
+
+for (i = 0; passw < 2772; i++)
+{
+n = rand() % 10;
+password[i] = seed[n];
+passw += password[i];
+}
+complem = 2772 - passw;
+password[i] = complem;
+printf("%s\n",  password);
+return (0);
+}
