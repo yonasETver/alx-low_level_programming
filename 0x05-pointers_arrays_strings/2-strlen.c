31e23f98a0e464182fc27f0948e30ef10f5e6607
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-18 12:52:24 +0300

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - a function to count the length
+* @s: variable
+* Return: return the length to the calling function
+
+int _strlen(char *s)
+{
+    int i = 1, len_count = 0;
+    char c = s[0];
+    
+    for (;c != '\0'; c = s[i++])
+        len_count++;
+        return (len_count);
+ }
