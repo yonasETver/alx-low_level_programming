e6b08f04120710e911e76f745e7e99c2c1a64054
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-18 13:22:07 +0300

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - function prints a string, in reverse order
+ * str_len - used to count string length
+ * @str: variable of string
+ */
+void print_rev(char *str)
+{
+	int str_len = 0;
+	
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		str_len++;
+	}
+
+	int j = (str_len - 1);
+  while (j >= 0)
+	{
+		_putchar(s[n]);
+    j--;
+	}
+	_putchar('\n');
+}
