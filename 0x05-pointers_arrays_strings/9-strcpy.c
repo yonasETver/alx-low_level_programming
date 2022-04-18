21fef29727ad3892b32134c136cf852fa1fdc497
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-18 14:45:28 +0300

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - function copies the string value
+ * @char_cop: copy of char_org
+ * @char_org: original
+ * Return: return the copied reference to the calling
+ */
+char *_strcpy(char *char_copy, char *char_org)
+{
+	int i = 0;
+
+	while (char_org[i] != '\0')
+	{
+		char_copy[i] = char_org[i];
+    i++;
+	}
+	return (char_copy);
+}
