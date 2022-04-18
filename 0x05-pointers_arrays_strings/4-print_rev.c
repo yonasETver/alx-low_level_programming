1c69c54676fac0bb135315ce88a1f4bf90e926d4
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-18 14:08:46 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,16 @@
  */
 void print_rev(char *str)
 {
-	int str_len = 0, i =0, j;
-	
+	int str_len = 0;
+	int i = 0, j;
+
 	while (str[i] != '\0')
 	{
 		str_len++;
 		i++;
 	}
+	j = str_len - 1;
 
-	j = (str_len - 1);
-	
 	while (j >= 0)
 	{
 		_putchar(str[j]);
