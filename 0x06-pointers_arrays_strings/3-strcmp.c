bbca7a3745d599e88373a76f3272123df055fa21
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-20 23:57:19 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0;
-  int i = 0, result;
-  
-  while (s1[i] != '\0')
-  {
-  if (s1[i] == s2[i])
-  {
-    result = (s1[i] - '0') - (s2[i] - '0');
-  }
-  else
-  {
-    result = (s1[i] - '0') - (s2[i] - '0');
-    break;
-  }
-  i++;
+	int i = 0, result;
+	
+	while (s1[i] != '\0')
+		if (s1[i] == s2[i])
+		{
+			result = (s1[i] - '0') - (s2[i] - '0');
+		}
+	else
+	{
+		result = (s1[i] - '0') - (s2[i] - '0');
+		break;
+	}
+	i++;
 }
 
 return (result);
