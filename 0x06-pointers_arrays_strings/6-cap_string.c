3e8199da52b61fa67b27707af4237ca63d30236c
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-21 00:16:42 +0300

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * cap_string - a function that capitalizes all words of a string.
+  * @s: a string variable
+  * Return: return capitalizes string.
+  */
+char *cap_string(char *s)
+{
+	int i = 0 , j = 0;
+	int cap_cspc = 13;
+	char str_spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[j])
+	{
+		for (; i < cap_cspc; i++)
+		{
+			if ((j == 0 || s[j - 1] == str_spc[i]) && (s[j] >= 97 && s[j] <= 122))
+				s[j] -= 32;
+		}
+
+		j++;
+	}
+
+	return (s);
+}
