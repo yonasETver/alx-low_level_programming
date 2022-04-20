7f4bb11499195c3d4e900ec8ba5bf107426c5e04
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-20 23:45:42 +0300

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * string_toupper - function that change lowercase to upper
+  * @p: the lowercase string variable
+  * Return: the variable all in uppercase
+  */
+char *string_toupper(char *p)
+{
+	int i = 0;
+
+	while (p[i] != '\0')
+	{
+		if (p[i] >= 97 && p[i] <= 122)
+		{
+			p[i] -= 32;
+		}
+    i++;
+	}
+
+	return (p);
+}
