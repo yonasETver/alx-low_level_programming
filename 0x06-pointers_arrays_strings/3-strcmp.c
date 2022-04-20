fb2a166c730ca4482267c0517ffd4f3f8f8491eb
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-20 23:27:40 +0300

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcmp - function that compare two string.
+ * @s1: is a pointer string variable
+ * @s2: is a pointer string  variable 
+ * Return: return a result.
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0, result;
+
+while (s1[i] != '\0')
+{
+  if (s1[i] == s2[i])
+  {
+    result = (s1[i] - '0') - (s2[i] - '0');
+  }
+  else
+  {
+    result = (s1[i] - '0') - (s2[i] - '0');
+    break;
+  }
+  i++;
+}
+
+return (result);
+}
