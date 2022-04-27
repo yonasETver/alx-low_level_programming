abc37c7f6929d4f345ef4c7224ca12fc48866a57
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-04-27 12:15:12 +0300

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+int tmp_prime(int n, int i);
+
+/**
+ * divisors - number is prime?
+ * @n: integer params
+ * @m: integer params
+ * Return: boolean
+ */
+
+int divisors(int n, int m);
+
+/**
+ * is_prime_number -a function that returns 1 if the input integer is a prime number, otherwise return 0
+ * @n: integer params
+ * Return: recursion
+ */
+
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divisors(3, n));
+	}
+}
+
+int divisors(int n, int m);
+{
+	if (m % n == 0)
+	{
+		return (0);
+	}
+	else if (m / 2 > n)
+	{
+		return (divisors(n + 2, m));
+	}
+	else
+	{
+		return (1);
+	}
+}
