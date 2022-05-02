7995951f0f8e23e7227963603753ffaa6fe7fc48
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-02 20:04:42 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -17,7 +17,7 @@ char **strtow(char *str)
 		return (NULL);
 	}
 
-	for (;*(str + i); i++)
+	for (; *(str + i); i++)
 	{
 		if (*(str + i) != ' ')
 		{
@@ -44,39 +44,29 @@ char **strtow(char *str)
 	while (*str)
 	{
 		while (*str == ' ' && *str)
-		{
 			str++;
-		}
 		len = 0;
 
 		while (*(str + len) != ' ' && *(str + len))
-		{
 			len += 1;
-		}
 		len += 1;
 		col = malloc(sizeof(char) * len);
 
 		if (!col)
 		{
 			for (k = j - 1; k >= 0; k--)
-			{
 				free(f[k]);
-			}
 			free(f);
 			return (NULL);
 		}
 
 		for (k = 0; k < (len - 1);  k++)
-		{
 			*(col + k) = *(str++);
-		}
 		*(col + k) = '\0';
 		*(f + j) = col;
 
 		if (j < (count - 1))
-		{
 			j++;
-		}
 	}
 	*(f + j) = NULL;
 	return (f);
