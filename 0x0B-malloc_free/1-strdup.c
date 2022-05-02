8e236cc3599b86181b2b2bca07c07bb713500c25
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-02 19:47:06 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,52 +1,32 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include "main.h"
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
- */
-
-
-int _strlen(char *s)
-{
-int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
-}
-
-/**
- * *str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
+ * _strdup - copy
+ * @str: pointer to string
  * Return: pointer
  */
 
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-int size1, size2, i;
-char *m;
+	char *s;
+	int i = 0, j;
 
-if (s1 == NULL)
-	s1 = "\0";
-if (s2 == NULL)
-	s2 = "\0";
+	if (!str)
+		return (NULL);
 
-size1 = _strlen(s1);
-size2 = _strlen(s2);
-m = malloc((size1 + size2) *sizeof(char) + 1);
-if (m == 0)
-	return (0);
+	while (*(str + i))
+		i++;
+	i++;
+	s = malloc(sizeof(char) * i);
 
-for (i = 0; i <= size1 + size2; i++)
-{
-	if (i < size1)
-		m[i] = s1[i];
-	else
-		m[i] = s2[i - size1];
-}
-m[i] = '\0';
-return (m);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		s[j] = str[j];
+	}
+	return (s);
 }
