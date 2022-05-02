a5c5494e0b98075f4d891082539b18d9e8ca3d5e
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-02 23:32:11 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,24 +1,17 @@
 #include "main.h"
-
 /**
- * len - returns length of str
- *@str: string to be counted
- *
- * Return: length of the string
- */
-
+* len - returns length of str
+*@str: string to be counted
+* Return: length of the string
+*/
 int len(char *str)
 {
 	int len = 0;
-
 	if (str != NULL)
-	{
 		while (str[len])
 			len++;
-	}
 	return (len);
 }
-
 /**
  * num_words - counts the number of words in str
  *@str: string to be used
@@ -28,26 +21,20 @@ int len(char *str)
 int num_words(char *str)
 {
 	int i = 0, words = 0;
-
 	while (i <= len(str))
 	{
 		if ((str[i] != ' ') && (str[i] != '\0'))
-		{
 			i++;
-		}
 		else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
 		{
 			words += 1;
 			i++;
 		}
 		else
-		{
 			i++;
-		}
 	}
 	return (words);
 }
-
 /**
  *strtow - splits a stirng into words
  *@str: string to be splitted
