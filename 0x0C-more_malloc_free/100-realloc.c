478ab68cc096698311a8c205129b1496af770b8b
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-06 21:33:05 +0300

    Create 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+/**
+ * _realloc - a function that reallocates a memory block using malloc and free
+ * @ptr: void pointer parameter
+ * @old_size: int parameter
+ * @new_size: int parameter
+ * Return:void ppointer
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i = 0;
+	void *new_ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return ('\0');
+
+		return (new_ptr);
+	}
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return ('\0');
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (!new_ptr)
+  {
+		return ('\0');
+  }
+
+	for (; i < old_size; i++)
+  {
+		*((char *)new_ptr + i) = *((char *)ptr + i);
+  }
+
+	free(ptr);
+	return ((void *)new_ptr);
+}
