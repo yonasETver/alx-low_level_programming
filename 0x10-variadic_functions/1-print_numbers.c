c2c2016b21704bb6a63efd6ceb5d2b8fb4d5d170
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-12 18:15:23 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - a function that prints strings, followed by a new line.
+ * print_numbers - a function that prints numbers, followed by a new line.
  * @separator: separe numbers
  * @n: int, number of undefined arguments
  */
