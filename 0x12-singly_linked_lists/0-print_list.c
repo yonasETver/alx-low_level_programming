3fb4fc69da3baebf0e24ee26671f75f445c52617
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-19 18:31:23 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
 /**
-* print_list - a function that prints elements of a list_t list.
-* @h: linked list structure
-* Return: number of nodes of the linked list
-*/
+ * print_list - print elements of linkedlist
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
 
 size_t print_list(const list_t *h)
 {
