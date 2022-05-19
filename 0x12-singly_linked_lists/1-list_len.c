d70a867213e512f517b813f142002d93804ed3f3
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-19 18:04:25 +0300

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - a function that returns the number of elements in a linked list_t list.
+ *@h: const list_t pointer to head of list
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+    i++;
+	}
+	return (i);
+}
