477638d65f207c06e8d8c6fc4f6421baf4ae810e
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-19 18:33:39 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,16 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nbr_nodes = 0;
-	const list_t *current = h;
+	int i = 0;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", current->len,
-		current->str != NULL ? current->str : "(nil)");
-		current = current->next;
-		nbr_nodes++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
 	}
-	return (nbr_nodes);
+	return (i);
 }
