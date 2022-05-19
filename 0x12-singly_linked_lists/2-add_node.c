f707870608adfdca4ca6297f7b3268cfbe142c88
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-19 18:14:45 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -25,7 +25,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	newhead->str = nstr;
-	newhead->len = x;
+	newhead->len = i;
 	newhead->next = *head;
 	*head = newhead;
 	return (newhead);
