cb10130dd5bcdac7537974542e4d98202e84cb12
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-19 17:53:00 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include<stddef.h>
+#include<stdarg.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
  * struct list_s - singly linked list structure 
