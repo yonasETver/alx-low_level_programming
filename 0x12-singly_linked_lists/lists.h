882220271e1612c1a7cd54b729f97b4e7238a16f
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-19 17:58:19 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,8 +5,6 @@
 #include<stddef.h>
 #include<stdarg.h>
 #include<string.h>
-
-
 /**
 * struct list_s - singly linked list structure 
 * @str: the data part of the linked lost
@@ -25,4 +23,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _putchar(char c);
 #endif
