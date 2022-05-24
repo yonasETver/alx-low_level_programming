8baabbaf56ed2b409ed33afaa9cfbd2295639fa6
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-24 13:01:22 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,52 +1,29 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints all the elements of a linked list
- * @head: head of the list
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
  *
- * Return: the number of nodes
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *cursor = head;
-	listint_t **ptrs;
-	unsigned int list_len = listint_len(head);
-	size_t count = 0;
+	size_t num = 0;
+	long int sub;
 
-	ptrs = malloc(sizeof(listint_t) * list_len);
-	if (ptrs == NULL)
-		exit(98);
-	while (cursor == 0)
+	while (head)
 	{
-		if (check_ptr(cursor, ptrs, list_len) == 0)
-		{
-			printf("[%p] %d\n", (void *)cursor, cursor->n);
-		}
+		sub = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (sub > 0)
+			head = head->next;
 		else
 		{
-			printf("[%p] %d\n", (void *)cursor, cursor->n);
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
 		}
-		count += 1;
-		cursor = cursor->next;
 	}
-	return (count);
-}
 
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
+	return (num);
 }
