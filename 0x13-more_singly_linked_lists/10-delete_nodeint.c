bcc05b781d9793f8a45ad35dda0296b6e4d930b3
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-24 12:25:47 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,9 +12,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current, *next;
 
 	if (head == NULL || *head == NULL)
-  {
+	{
 		return (-1);
-  }
+	}
 	if (index == 0)
 	{
 		next = (*head)->next;
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (current->next == NULL)
 			return (-1);
 		current = current->next;
-    i++;
+		i++;
 	}
 	next = current->next;
 	current->next = next->next;
