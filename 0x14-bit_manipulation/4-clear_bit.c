adc1f03828c7c9e483d0b2ba3360ae132dcacc18
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-26 16:48:04 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
-* clear_bit - sets the value of a bit to 0 at a given index
-* @n: long int parameter
-* @index: int index parameter
-* Return: 1 or -1 
-*/
-
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: parameter
+ * @index: index
+ * Return: 1 if works, -1 if error
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * BYTE_SIZE))
