cd88d17ee00b6424ba03f953de035da36afbb578
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-26 16:04:31 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define BYTE_LENGHT 8
 
 /*
 * Function declaration
 */
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
