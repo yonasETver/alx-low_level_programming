b1b73ab6baf7282bc2e3f9a2487ec88a04640d58
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-26 16:02:08 +0300

    Update 0-binary_to_unit.c

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,26 +1,48 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * binary_to_uint - a function converts a binary number to unsigned int
- * @b: parameter 
- * Return: the converted number
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: void
+*/
+
+
+int _strlen(const char *s)
+{
+int i = 0;
+while (s[i])
+	i++;
+
+return (i);
+}
+
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary number
+ *
+ * Return: 0 or converted number
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int i = 0;
+	unsigned int n = 0;
+	int i, len;
 
-	if (!b)
-	{
+	if (b == NULL)
 		return (0);
+
+	len = _strlen(b);
+
+	for (i = 0; i != len; i++)
+	{
+		if (b[len - i - 1] == '1')
+			n += 1 << i;
+		else if (b[len - i - 1] != '0')
+			return (0);
 	}
 
-	while (b[i])
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		decimal = 2 * decimal + (b[i] - '0');
-		i++;
-	}
-	return (decimal);
+	return (n);
 }
