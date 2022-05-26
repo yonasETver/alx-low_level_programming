dcb751b62719ab4101d72e527f490e5f72de4e72
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-05-26 16:24:05 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,7 @@
 #include "main.h"
 
-
-/**
- * _strlen - find the length of a string
- * @s: pointer to the string to check
- * Return: void
-*/
-
-
-int _strlen(const char *s)
-{
-int i = 0;
-while (s[i])
-	i++;
-
-return (i);
-}
+//function declaration
+int _strlen(const char *s);
 
 
 /**
@@ -28,14 +14,16 @@ return (i);
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
-	int i, len;
+	int i = 0, len;
 
 	if (b == NULL)
+	{
 		return (0);
+	}
 
 	len = _strlen(b);
 
-	for (i = 0; i != len; i++)
+	for (; i != len; i++)
 	{
 		if (b[len - i - 1] == '1')
 			n += 1 << i;
@@ -44,4 +32,19 @@ unsigned int binary_to_uint(const char *b)
 	}
 
 	return (n);
+}
+
+/**
+ * _strlen - function return length of a string
+ * @s: string parameter
+ * Return: length
+*/
+
+int _strlen(const char *s)
+{
+int i = 0;
+while (s[i])
+	i++;
+	
+return (i);
 }
